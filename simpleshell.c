19258ee934caeb9b471bbe7e010287ea042bf6f8
Isaac <isaac052503@gmail.com>
2024-08-24 16:13:08 -0300

    init simpleshell.c

--- simpleshell.c
+++ simpleshell.c
@@ -1,9 +1,11 @@
 /*****************************************************************
-Nome: Anderson Murillo          RA: 248221
-Nome:                           RA:
+Nome: Anderson Murillo                  RA: 248221
+Nome: Isaac do Nascimento Oliveira      RA: 247175
 *******************************************************************/
 
 #include "stdio.h"
+#include "unistd.h"
 
-
-
+int main(int argc, char *argv[]) {
+   
+}
