d71b56f2f25ebdaf1c8f8278407b633cca4fada4
Anderson Murillo Tito Lira Leite <ra248221@dac.unicamp.br>
2024-08-24 20:48:13 -0300

    "criei uma funcao que concatena o comando com o diretorio para poder tentar executa-lo no execv posteriormente"

--- simpleshell.c
+++ simpleshell.c
@@ -11,6 +11,7 @@ Nome: Isaac do Nascimento Oliveira      RA: 247175
 #define MAX_CMD_LEN 256 // max cmd or args len
 #define MAX_ARGS_LEN 256 // max cmd or args len
 
+
 void read_command(char *cmd, char *args[]) {
     char line[MAX_LINE_LEN];
 
@@ -27,17 +28,32 @@ void read_command(char *cmd, char *args[]) {
         args[argcount++] = strtok(line, " ");
         token = args[argcount-1];
     }
-
 }
 
-
-void verify_command(char *cmd, char *dir) {
-    
+// returns the cmd concatenated with the dir
+char *concatenate_cmd_with_dir(char *cmd, char *dir) {
+    // '/usr/bin/ -> example of dir
+    // 'whoiam' -> example of cmd
+    // in the end, i have to verify if this kind of thing occurs:
+    // '/usr/bin/whoiam'
+    char *dir_with_bar, result;
+    strcpy(dir_with_bar, strcat(dir, '/'));
+    strcpy(result, strcat(dir_with_bar, cmd));
+    return result;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
+    // argv is the first user input
+    // cmd is the command
+    // args is what cmd have to do
     char cmd[MAX_CMD_LEN];
     char *args[MAX_ARGS_LEN];
+    char argv[MAX_LINE_LEN];
+
+    // the argv[1] is not the whole dir, we have to separate
+    // the strings after the blank space to get the dir and work
+    // with it
+    fgets(argv, sizeof(argv), stdin);
 
     while (1) {
         int status;
@@ -46,17 +62,18 @@ int main(int argc, char *argv[]) {
         // read and check input
         read_command(cmd, args); /* reads cmd and args
          from command line */
-        verify_command(cmd, argv[1]);
-        if (!verify_command) {
-            printf("ERROR: Invalid command.");
-            continue;
-        }
+        char *result = concatenate_cmd_with_dir(cmd, argv[1]);
         
         // valid command
         pid = fork();
         if (pid == 0) {
-            execv(cmd, args);
-            exit(1);
+            int sucess = execv(result, args);
+            if (sucess == -1) {
+                printf("ERROR: Invalid command.");
+                continue;
+            } else {
+                exit(1);
+            } 
         }
         else {
             wait(&status);
